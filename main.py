1d3058de73e1445641d60a4ec5b56163905a2aea
Nabil Koroghli <nabil@edgeimpulse.com>
2021-12-01 15:20:24 +0100

    simplify

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 python_cmd = os.path.join(dir_path, "sub-process", ".venv/bin/python")
 result = subprocess.run(
-    f"bash -c '{python_cmd} sub.py'", capture_output=True, shell=True, cwd="sub-process"
+    f"{python_cmd} sub.py", capture_output=True, shell=True, cwd="sub-process"
 )
 print(str(result.stdout))
